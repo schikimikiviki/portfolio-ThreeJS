6fabb222fc9790399350329f6ecf5497ccf9523b
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-07-16 21:14:01 +0200

    repo initialization

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+
+  return (
+    <div>
+      <nav
+        className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg primary`}
+      >
+        <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={logo} alt="logo" className="w-3 h-3 object-contain" />
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
