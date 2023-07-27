25a9722ffa87a7f2a7e2a70b8724049dd71bf424
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-07-27 18:50:27 +0200

    alien working

--- src/components/Alien.jsx
+++ src/components/Alien.jsx
@@ -1,42 +1,57 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import {
-  OrbitControls,
-  Preload,
-  useGLTF,
-  useAnimations,
-} from "@react-three/drei";
+import React, { Suspense, useEffect } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import Loader from "./Loader";
+import { AnimationMixer } from "three";
+
+let mixer = null;
 
 const Alien = () => {
   const gltf = useGLTF("./alien/scene.gltf");
-  const { nodes, animations } = gltf;
-  const { actions } = useAnimations(animations, nodes);
 
-  // Play the animation on mount
-  React.useEffect(() => {
-    actions && actions["mixamo.com"].play().setLoop(THREE.LoopRepeat);
-  }, [actions]);
+  useEffect(() => {
+    const model = gltf.scene;
+    const animations = gltf.animations;
 
-  return <primitive object={gltf.scene} scale={2.5} position-y={0} />;
+    if (model && animations && mixer === null) {
+      mixer = new AnimationMixer(model);
+
+      const action = mixer.clipAction(animations[0]);
+      action.play();
+    }
+  }, [gltf]);
+
+  useFrame((_, delta) => {
+    if (mixer) {
+      mixer.update(delta);
+    }
+  });
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive
+        object={gltf.scene}
+        scale={5}
+        position={[0.6, -3, 0.08]}
+        // rotation={[-0.3, -0.5, 0]}
+      />
+    </mesh>
+  );
 };
 
 const AlienCanvas = () => {
   return (
     <Canvas
       shadows
-      //   frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
       camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
     >
       <ambientLight intensity={0.5} />
       <directionalLight intensity={1} position={[5, 10, 5]} castShadow />
       <Suspense fallback={<Loader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        <OrbitControls enableZoom={false} />
         <Alien />
       </Suspense>
     </Canvas>
