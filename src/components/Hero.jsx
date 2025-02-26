2fc2ae7f7c3ba0cb84aac6b2485604fbc913a35f
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-02-26 11:30:02 +0100

    small change in text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { motion } from "framer-motion";
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className='relative w-full h-screen mx-auto'>
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#DB5461]" />
-          <div className="w-1 sm:h-80 h-40 pink-gradient" />
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#DB5461]' />
+          <div className='w-1 sm:h-80 h-40 pink-gradient' />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi! <span className="text-[#915eff]"> </span>
+            Hi! <span className='text-[#915eff]'> </span>
           </h1>
           <p
             className={`${styles.heroSubText} mt-2 text-white-100 text-justify`}
           >
-            I'm a junior developer based in Vienna. I'm always looking for
+            I'm a fullstack developer based in Vienna. I'm always looking for
             challenges and I enjoy diving into new technologies and making them
             work for me, my team and my organization.
           </p>
@@ -29,17 +29,17 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: "loop",
+                repeatType: 'loop',
               }}
-              className="rounded-full w-2 h-3 bg-secondary mb-1"
+              className='rounded-full w-2 h-3 bg-secondary mb-1'
             />
           </div>
         </a>
